89150cc32eff5c67d7d47fded3b0258b9af8bbfc
Sefa Baygül <sefa.baygul06@gmail.com>
2022-08-17 13:19:05 +0300

    I5

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -3,6 +3,7 @@ public class GitIgnore {
         System.out.println("gitignore denem 1");
         System.out.println("gitignore denem 3 main");
         System.out.println("gitignore denem 2 branch");
+        System.out.println("gitignore denem 4 main");
 
     }
 }
